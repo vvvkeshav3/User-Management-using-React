c34e5688bd1ee6facaccb12e3238de55b507cf88
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-05-30 19:55:45 +0530

    Added React Portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import classes from './ErrorModal.module.css';
-import Card from './Card';
-import Button from './Button';
+import Backdrop from './Backdrop';
+import ModalOverlay from './ModalOverlay';
+
+import ReactDOM from 'react-dom';
 
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onCancelError}/>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onCancelError}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onCancelError}/>,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay title ={props.title} message = {props.message} onClick={props.onCancelError}/>,
+        document.getElementById('overlay-root')
+      )}
+    </React.Fragment>
   );
 };
 
